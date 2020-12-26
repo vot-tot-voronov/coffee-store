c5af9ecfba09131d292751efbc59864c3cabf0f1
Ivan <frontenddeveloperweb@gmail.com>
2020-12-26 23:48:30 +0500

    Connected redux store with the app

--- client/src/components/pages/home-page.js
+++ client/src/components/pages/home-page.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const HomePage = () => {
-    return <h1>Home Page</h1>
+import { connect } from 'react-redux';
+
+const HomePage = ({ coffee }) => {
+    console.log(coffee);
+    return (
+        <div>
+            <h1>Кофе: {coffee[0].name}</h1>
+            <p>Страна происхождения: {coffee[0].country}</p>
+            <p>Цена: {coffee[0].price}</p>
+        </div>
+    );
 };
 
-export default HomePage;
+const mapDispatchToProps = (state) => {
+    return {
+        coffee: state.coffee
+    }
+}
+
+export default connect(mapDispatchToProps)(HomePage);
