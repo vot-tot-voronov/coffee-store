3358246d2085bb9b86f45ec81f8e65f6d8166af4
Ivan <frontenddeveloperweb@gmail.com>
2021-01-19 23:34:28 +0500

    Created new component Delivery

---
+++ client/src/components/delivery/delivery.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './delivery.scss';
+
+const Delivery = () => {
+
+    return (
+        <div className="delivery">
+            Доставка
+        </div>
+    );
+}
+
+export default Delivery;
