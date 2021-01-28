2a30c7aab6bd0a4106ef175b6590e89219f409c5
Ivan <frontenddeveloperweb@gmail.com>
2021-01-28 14:34:56 +0500

    Changed and added some styles

--- client/src/components/cart-list-item/cart-list-item.js
+++ client/src/components/cart-list-item/cart-list-item.js
@@ -12,23 +12,23 @@ const CartListItem = ({cartItem, onIncrease, onDecrease, onDelete}) => {
                 </div>
             </div>
             <div className="col-1-of-6">
-                <h3 className="cart-item__title">{name}</h3>
+                <h3 className="cart-item__title cart-item--line-height">{name}</h3>
             </div>
             <div className="col-1-of-6">
-                <h3 className="cart-item__price">Цена: {price}</h3>
+                <p className="cart-item__price cart-item--line-height">Цена: {price}</p>
             </div>
             <div className="col-1-of-6">
                 <div className="cart-item__manage">
-                    <button onClick={onDecrease} className="cart-item__manage--remove">-</button>
+                    <button onClick={onDecrease} className="btn btn--manage cart-item__manage--remove">-</button>
                     <div className="cart-item__manage--counter">{count}</div>
-                    <button onClick={onIncrease} className="cart-item__manage--add">+</button>
+                    <button onClick={onIncrease} className="btn btn--manage cart-item__manage--add">+</button>
                 </div>
             </div>
             <div className="col-1-of-6">
-                <p className="cart-item__total">Итого: {count * price}</p>
+                <p className="cart-item__total cart-item--line-height">Итого: {count * price}</p>
             </div>
             <div className="col-1-of-6">
-                <button onClick={onDelete} className="cart-item__remove-item">X</button>
+                <button onClick={onDelete} className="btn btn--remove cart-item__remove-item">X</button>
             </div>
         </div>
     );
