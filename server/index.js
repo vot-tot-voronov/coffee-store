c51746557b851a1dab856aaac7d100c56aed5c4b
Ivan <frontenddeveloperweb@gmail.com>
2021-02-01 12:36:36 +0500

    Writing backend. Connected to db

--- server/index.js
+++ server/index.js
@@ -1,11 +1,30 @@
-const fastify = require('fastify')()
+require('dotenv').config()
+const express = require('express')
+const sequelize = require('./db')
+const models = require('./models/models')
+const cors = require('cors')
 
-fastify.register(require('./routes/route'))
+const PORT = process.env.PORT || 5000
 
-fastify.listen(3001, (err, address) => {
-    if (err) {
-        fastify.log.error(err)
-        process.exit(1)
-    }
-    fastify.log.info(`server listening on port ${address}`)
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'working'})
 })
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+
+start()
+
